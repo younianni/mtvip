379af3c82f18b9183bf88b1f8f3a51e5b1c238ac
younianni <yanzhengni@outlook.com>
2025-06-14 13:12:07 -0300

    Update price-monitor.js

--- .github/workflows/price-monitor.js
+++ .github/workflows/price-monitor.js
@@ -305,7 +305,7 @@ async function sendPriceChangeEmail(changes) {
     to: config.email.to,
     subject: 'MT卡密价格变动通知',
     html: `
-      <h3>MT卡密价格变动通知</h3>
+      <h3>MT卡密价格变动通知-https://younianni.github.io/mtvip/</h3>
       <p>以下商品价格发生了变动:</p>
       ${priceChangesHtml}
       <p>当前时间: ${new Date().toLocaleString()}</p>
